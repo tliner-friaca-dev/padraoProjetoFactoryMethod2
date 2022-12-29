4445e33f638c20ed3fbbd6f18a1123124f9eeaa1
tliner-friaca-dev <tlinerfc@hotmail.com>
2022-12-28 21:56:09 -0300

    first commit

---
+++ src/test/java/br/com/projeto/padraoProjetoFactoryMethod/PadraoProjetoFactoryMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.padraoProjetoFactoryMethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class padraoProjetoFactoryMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
